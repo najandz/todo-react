1a06b032ca8134af00ecd0392bcd445065b70630
Nazhand Zareei <nazhand.zareei@trader.ca>
2019-02-05 19:19:03 -0800

    First Commit

---
+++ build/precache-manifest.d000cc46505f299e6a4967959ea88551.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08a911c21192766317f6",
+    "url": "//todo-react/static/css/main.9f62e461.chunk.css"
+  },
+  {
+    "revision": "08a911c21192766317f6",
+    "url": "//todo-react/static/js/main.08a911c2.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "//todo-react/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "004474138f0ca349ae81",
+    "url": "//todo-react/static/js/runtime~main.00447413.js"
+  },
+  {
+    "revision": "285afe86b3954fdbe36bc5e77ed057f6",
+    "url": "//todo-react/index.html"
+  }
+];
